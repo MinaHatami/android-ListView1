8bd169609d1286fca023cd5752d02ae18e18f022
mina <mina@MINAPC>
2015-06-07 12:36:03 -0400

    load selfies from gallery/myselfies

--- src/com/minahatami/listview1/MainActivity.java
+++ src/com/minahatami/listview1/MainActivity.java
@@ -1,10 +1,14 @@
 package com.minahatami.listview1;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -21,11 +25,17 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		myPics.add(new Pic(R.drawable.ic_launcher, "documents/pictures", "img0"));
+		/*myPics.add(new Pic(R.drawable.ic_launcher, "documents/pictures", "img0"));
 		myPics.add(new Pic(R.drawable.ic_launcher, "documents/pictures", "img1"));
 		myPics.add(new Pic(R.drawable.ic_launcher, "documents/pictures", "img2"));
-		myPics.add(new Pic(R.drawable.ic_launcher, "documents/pictures", "img3"));
+		myPics.add(new Pic(R.drawable.ic_launcher, "documents/pictures", "img3"));*/
 
+		List<String> selfies = getSelfies();
+
+		for (String file : selfies) {
+			myPics.add(new Pic(0, file, file));
+		}
+		
 		populateListView();
 	}
 
@@ -57,12 +67,45 @@ public class MainActivity extends Activity {
 			TextView path = (TextView) myView.findViewById(R.id.path);
 			TextView name = (TextView) myView.findViewById(R.id.name);
 
-			image.setImageResource(currentPic.getImage());
+			Bitmap bm = getBitmap(currentPic.getPath());
+			image.setImageBitmap(bm);
 			path.setText(currentPic.getPath());
 			name.setText(currentPic.getName());
 
 			return myView;
 		}
 
+	}
+	
+	private Bitmap getBitmap(String path)
+	{
+		return BitmapFactory.decodeFile(path);
+	}
+
+	public File getRootDirectoy() {
+		return Environment
+				.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+	}
+
+	// Get the selfie photos from a folder called "My Selfies" and save them in a list called "selfieItems".
+	public List<String> getSelfies() {
+		final String pathName = "My Selfies";
+		List<String> selfieItems = new ArrayList<String>();
+		final File selfieFolder = new File(getRootDirectoy(), pathName);
+
+		if (!selfieFolder.exists()) {
+			selfieFolder.mkdir();
+		}
+		else {
+			File[] files = selfieFolder.listFiles();
+			for (File file : files) {
+				if (!file.isDirectory()
+						&& file.getAbsolutePath().endsWith(".jpg")) {
+					selfieItems.add(file.getAbsolutePath());
+				}
+			}
+		}
+
+		return selfieItems;
 	}
 }
